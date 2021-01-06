15a53cf4b5ad18baec48f36f680382c6e17b2386
曾科凡 <zeng.kefan@intellif.com>
2021-01-06 14:03:54 +0800

    项目搭建

---
+++ webapp/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Home from "@src/page/pageHome";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Route
+        path="/"
+        exact
+        render={() => <Redirect to="/home" push />}
+      ></Route>
+      <Route path="/home" exact component={Home}></Route>
+    </Router>
+  );
+}
+
+export default App;
